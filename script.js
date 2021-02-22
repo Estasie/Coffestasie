1266ec6362703d846f7c455faea9214c57424fc1
Anastasia <anastasiaberia00@gmail.com>
2021-02-23 01:34:08 +0700

    Cookie animation in process

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let blockInfo = document.querySelectorAll(".container__block");
+  let form = document.querySelector("#emploersForm");
+  let emploersBlock = document.querySelector("#emploers");
+  let cookieButton = document.querySelector('.form__button');
 
-
-
-window.addEventListener('DOMContentLoaded', () => {
-    let blockInfo = document.querySelectorAll('.container__block');
-    let form = document.querySelector('#emploersForm');
-    let emploersBlock = document.querySelector('#emploers');
-    
-
+  function videoPausePlay() {
     for (let elem of blockInfo) {
-        let video = document.querySelectorAll('#videoPlay');
-        video.forEach(el => {
-            elem.addEventListener('mouseover', () => {
-                el.play();
-            });
-            elem.addEventListener('mouseout', () => {
-                el.pause();
-            });
-        })
-
+      let video = document.querySelectorAll("#videoPlay");
+      video.forEach((el) => {
+        elem.addEventListener("mouseover", () => {
+          el.play();
+        });
+        elem.addEventListener("mouseout", () => {
+          el.pause();
+        });
+      });
     }
+  }
 
-    emploersBlock.addEventListener('click', () => {
-        emploersBlock.classList.add('hidden');
-        form.classList.remove('hidden');
-        form.classList.add('flex');
-    });
+  function openForm(){
+    emploersBlock.addEventListener("click", () => {
+        emploersBlock.classList.add("hidden");
+        form.classList.remove("hidden");
+        form.classList.add("flex");
+      });
+  }
+  
+  let moveStep=0;
+  let wdth = screen.width - 500;
+  let reverse = false;
 
-    function validateForm() {
-        
-        let data = [document.forms["empForm"]["Name"],document.forms["empForm"]["Tel"], document.forms["empForm"]["Mail"]];
-        console.log(data);
-        console.log(data[0].value);
-        // Check Name 
-        if (data[0].value === null || data[0].value === "" ){
-           console.log(`Заполните поле ${data[0].value}`);
+  function moveCookie(){
+    let stop;
+    let progr = 0;
+    let button = document.querySelector('.form__button');
+    function moveCook(timestamp){
+        if(progr > 350) {
+            cancelAnimationFrame(stopId);
+            return;
+        } else {
+            progr = progr +3;
+            button.style.left = progr + "px";
+            stopId= window.requestAnimationFrame(step);
         }
-        
     }
+      window.requestAnimationFrame(step);
+  }
 
-    validateForm();
+  
+  
 
-
-
+  videoPausePlay();
+  openForm();
+  moveCookie();
 
 
 });
