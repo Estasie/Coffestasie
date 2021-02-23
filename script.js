45241b6dc99cc0be57ba40b821d9dc8440604296
Anastasia <anastasiaberia00@gmail.com>
2021-02-23 23:49:22 +0700

    button animation process

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
   let blockInfo = document.querySelectorAll(".container__block");
   let form = document.querySelector("#emploersForm");
   let emploersBlock = document.querySelector("#emploers");
-  let cookieButton = document.querySelector('.form__button');
+  let cookieButton = document.querySelector(".form__button");
 
   function videoPausePlay() {
     for (let elem of blockInfo) {
@@ -18,41 +18,49 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  function openForm(){
+  function openForm() {
     emploersBlock.addEventListener("click", () => {
-        emploersBlock.classList.add("hidden");
-        form.classList.remove("hidden");
-        form.classList.add("flex");
-      });
+      emploersBlock.classList.add("hidden");
+      form.classList.remove("hidden");
+      form.classList.add("flex");
+    });
   }
-  
-  let moveStep=0;
+
+  let moveStep = 0;
   let wdth = screen.width - 500;
   let reverse = false;
 
-  function moveCookie(){
-    let stop;
-    let progr = 0;
-    let button = document.querySelector('.form__button');
-    function moveCook(timestamp){
-        if(progr > 350) {
-            cancelAnimationFrame(stopId);
-            return;
+  function moveCookie() {
+    cookieButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      let checkButtonClasses = e.target.classList.contains("_active");
+      let cookie = document.querySelector(".cookie");
+      let promiseForCookie = new Promise(() => {
+        if(!checkButtonClasses){
+          
+          setTimeout(function () {
+            e.target.classList.add("_active");
+            e.target.innerHTML = "";
+            cookie.classList.add("active__cookie");
+          }, 1000);
+          
         } else {
-            progr = progr +3;
-            button.style.left = progr + "px";
-            stopId= window.requestAnimationFrame(step);
+          setTimeout(function () {
+            e.target.classList.remove("_active");
+            e.target.innerHTML = "Отправить";
+          }, 505);
+          
         }
-    }
-      window.requestAnimationFrame(step);
-  }
+        
+      });
+      
 
-  
-  
+      
+      
+    });
+  }
 
   videoPausePlay();
   openForm();
   moveCookie();
-
-
 });
