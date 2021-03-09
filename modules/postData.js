696b33ff9ccfe292ca3e14e7f3d5a19eda14353c
Estasie <anastasiaberia00@gmail.com>
2021-03-09 11:32:54 +0300

    A few additional changes
    Preserved some modified form related files: PostData and responsive styles for the form.

--- modules/postData.js
+++ modules/postData.js
@@ -1,52 +1,50 @@
-import formValidate from './formValidate.js';
-import {postData} from '../modules/services.js';
+import formValidate from "./formValidate.js";
+import { postData } from "../modules/services.js";
 export default function forms(formsSelector) {
-    const forms = document.querySelectorAll(formsSelector);
-    const formButton = document.querySelector('#cookieButton');
-    const message = {
-        loading: 'Отправка данных...',
-        success: 'Спасибо! Скоро мы с вами свяжемся',
-        failure: 'Что-то пошло не так...'
-    };
+  const forms = document.querySelectorAll(formsSelector);
+  const formButton = document.querySelector("#cookieButton");
+  const message = {
+    loading: "Отправка данных...",
+    success: "Спасибо! Скоро мы с вами свяжемся",
+    failure: "Что-то пошло не так...",
+  };
 
-    // let validStatus = formValidate();
-    // console.log(validStatus);
-    forms.forEach(item => {
-        bindPostData(item);
-    });
+  // let validStatus = formValidate();
+  // console.log(validStatus);
+  forms.forEach((item) => {
+    bindPostData(item);
+  });
 
-    function bindPostData(form) {
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
+  function bindPostData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
 
-            
-            let statusMessage = document.createElement('div');
-            statusMessage.style.cssText = `
+      let statusMessage = document.createElement("div");
+      statusMessage.style.cssText = `
                 display: block;
                 margin: 0 auto;
             `;
-            form.insertAdjacentElement('afterend', statusMessage);
-        
-            const formData = new FormData(form);
-            const json = JSON.stringify(Object.fromEntries(formData.entries()));
-            console.log(json);
-            if (formValidate()){
-                
-                postData('http://localhost:3000/applicants', json)
-                .then(data => {
-                    // console.log(data);
-                    alert(message.success);
-                    statusMessage.remove();
-                }).catch(() => {
-                    alert(message.failure);
-                }).finally(() => {
-                    form.reset();
-                });
-            } else {
-                console.log(message.failure);
-            }
-           
-        });
-    }
+      form.insertAdjacentElement("afterend", statusMessage);
 
+      const formData = new FormData(form);
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+      console.log(json);
+      if (formValidate()) {
+        postData("http://localhost:3000/applicants", json)
+          .then((data) => {
+            // console.log(data);
+            alert(message.success);
+            statusMessage.remove();
+          })
+          .catch(() => {
+            alert(message.failure);
+          })
+          .finally(() => {
+            form.reset();
+          });
+      } else {
+        console.log(message.failure);
+      }
+    });
+  }
 }
--- modules/videoLaunch.js
+++ modules/videoLaunch.js
@@ -1,21 +1,19 @@
-export default function videoPausePlay(){
-  let blocks = document.querySelectorAll('.container__block');
-  blocks.forEach(el => {
-    el.addEventListener('mouseover', function() {
-      if(this.classList.contains('flex')){
+export default function videoPausePlay() {
+  let blocks = document.querySelectorAll(".container__block");
+  blocks.forEach((el) => {
+    el.addEventListener("mouseover", function () {
+      if (this.classList.contains("flex")) {
         return;
-      }else {
-        this.querySelector('video').play();
-      } 
-    });
-    el.addEventListener('mouseleave', function() {
-      if(this.classList.contains('flex')){
-        return;
-      }else {
-        this.querySelector('video').pause();
+      } else {
+        this.querySelector("video").play();
       }
     });
-  })
+    el.addEventListener("mouseleave", function () {
+      if (this.classList.contains("flex")) {
+        return;
+      } else {
+        this.querySelector("video").pause();
+      }
+    });
+  });
 }
-
-
